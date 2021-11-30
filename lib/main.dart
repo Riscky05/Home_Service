4c86fdbc6d2284349797dd222f50752277e0df74
Riscky05 <Berber05.deco@gmail.com>
2021-11-30 16:03:44 +0100

    push

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// ignore: unused_import
+import 'package:day35/pages/home.dart';
+// ignore: unused_import
+import 'package:day35/pages/select_service.dart';
+import 'package:day35/pages/start.dart';
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(MaterialApp(
+    home: StartPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
